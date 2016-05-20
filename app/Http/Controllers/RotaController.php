251474af7b98a06ee0a9ae9b84ac889c4df51d31
julianasobreira <juliana.univasf@gmail.com>
2016-05-20 17:14:53 -0300

    Testando Query em Relação ManytoMany

--- app/Http/Controllers/RotaController.php
+++ app/Http/Controllers/RotaController.php
@@ -11,13 +11,15 @@ use App\ Ponto;
 class RotaController extends Controller
 {
     public function getParadas(Request $request){
-    	$pontos = Ponto::where('nome', '=', $request->origem)->first();
+    	$pontos = Ponto::where('nome', '=', $request->origem)->get();
     	echo "<h2>Origem: " . $request->origem . "</h2>";
     	echo "HORÁRIO" . " - " . "ÔNIBUS" . " - " . "MOTORISTA" . "<br>";
-    	foreach($pontos->rotas as $rota) {
-		    // pivot attribute exists on related subject
-		    echo $rota->pivot->horario . " - " . $rota->onibus . " - " . $rota->motorista . "<br>";
-		}
+        foreach($pontos as $ponto) {
+        	foreach($ponto->rotas as $rota) {
+    		    // pivot attribute exists on related subject
+    		    echo $rota->pivot->horario . " - " . $rota->onibus . " - " . $rota->motorista . "<br>";
+    		}
+        }
 		    	
     }
 }

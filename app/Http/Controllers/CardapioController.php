e4d1d32e5526ff86e1af56ffc089539960277a35
Raphael Mendes <raphaelmendes18@gmail.com>
2016-05-30 22:53:38 -0300

    Query para buscar datas

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use \DateTime;
 use App\Cardapio;
 use Illuminate\Http\Request;
 
@@ -12,6 +12,9 @@ class CardapioController extends Controller
 {
     public function getCardapios($dt_start,$dt_end)
     {
-        return $dt_start;
+        $dt_start = new DateTime($dt_start);
+        $dt_end = new DateTime($dt_end);
+        $cardapios = Cardapio::whereDate('date','>=',$dt_start->format('Y-m-d'))->whereDate('date','<=',$dt_end->format('Y-m-d'));
+        print_r($cardapios);            
     }
 }

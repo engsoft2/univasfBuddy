4638a6763f1eb5bde7fd6fe30258bdd5a5265e5b
Raphael Mendes <raphaelmendes18@gmail.com>
2016-05-30 23:23:59 -0300

    CRUD CARDAPIOS - NAO FINALIZADO NECESSITA FRONT END PARA TESTE

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -17,4 +17,25 @@ class CardapioController extends Controller
         $cardapios = Cardapio::whereDate('date','>=',$dt_start->format('Y-m-d'))->whereDate('date','<=',$dt_end->format('Y-m-d'));
         print_r($cardapios);            
     }
+    
+    public function store(Request $request)
+    {
+        Cardapio::firstOrCreate(['date' => $request->date,
+        'type'=>$request->type,
+        'sld_crua'=>$request->sld_crua,
+        'sld_cozida'=>$request->sld_cozida,
+        'prt_principal'=>$request->prt_principal,
+        'guarnicao'=>$request->guarnicao,
+        'cereal'=>$request->cereal,
+        'leguminosa'=>$request->leguminosa,
+        'vegetariano'=>$request->vegetariano,
+        'sobremesa'=>$request->sobremesa,
+        'sopa'=>$request->sopa,
+        'bebida'=>$request->bebida]);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        
+    }
 }

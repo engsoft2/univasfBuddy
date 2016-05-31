e419cbb3b4b823b5ecd8e0f0d46ddd3851eebe99
Raphael Mendes <raphaelmendes18@gmail.com>
2016-05-30 23:25:14 -0300

    Merge ok

--- app/Http/Controllers/CardapioController.php
+++ app/Http/Controllers/CardapioController.php
@@ -21,21 +21,35 @@ class CardapioController extends Controller
     public function store(Request $request)
     {
         Cardapio::firstOrCreate(['date' => $request->date,
-        'type'=>$request->type,
-        'sld_crua'=>$request->sld_crua,
-        'sld_cozida'=>$request->sld_cozida,
-        'prt_principal'=>$request->prt_principal,
-        'guarnicao'=>$request->guarnicao,
-        'cereal'=>$request->cereal,
-        'leguminosa'=>$request->leguminosa,
-        'vegetariano'=>$request->vegetariano,
-        'sobremesa'=>$request->sobremesa,
-        'sopa'=>$request->sopa,
-        'bebida'=>$request->bebida]);
+                                'type'=>$request->type,
+                                'sld_crua'=>$request->sld_crua,
+                                'sld_cozida'=>$request->sld_cozida,
+                                'prt_principal'=>$request->prt_principal,
+                                'guarnicao'=>$request->guarnicao,
+                                'cereal'=>$request->cereal,
+                                'leguminosa'=>$request->leguminosa,
+                                'vegetariano'=>$request->vegetariano,
+                                'sobremesa'=>$request->sobremesa,
+                                'sopa'=>$request->sopa,
+                                'bebida'=>$request->bebida]
+                                );
     }
     
     public function update(Request $request, $id)
     {
-        
+        Cardapio::where('id', $id)->update([
+                                'date' => $request->date,
+                                'type'=>$request->type,
+                                'sld_crua'=>$request->sld_crua,
+                                'sld_cozida'=>$request->sld_cozida,
+                                'prt_principal'=>$request->prt_principal,
+                                'guarnicao'=>$request->guarnicao,
+                                'cereal'=>$request->cereal,
+                                'leguminosa'=>$request->leguminosa,
+                                'vegetariano'=>$request->vegetariano,
+                                'sobremesa'=>$request->sobremesa,
+                                'sopa'=>$request->sopa,
+                                'bebida'=>$request->bebida]
+                                );
     }
 }

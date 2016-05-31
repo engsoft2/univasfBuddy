02a16d74d2676b65717dfea93d41c247c939245b
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-31 09:58:33 -0400

    Applied fixes from StyleCI

--- database/migrations/2016_05_19_011243_create_pontos_table.php
+++ database/migrations/2016_05_19_011243_create_pontos_table.php
@@ -15,8 +15,8 @@ class CreatePontosTable extends Migration
         Schema::create('pontos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->decimal('lat', 20,17);
-            $table->decimal('lng', 20,17);
+            $table->decimal('lat', 20, 17);
+            $table->decimal('lng', 20, 17);
             $table->timestamps();
         });
     }
